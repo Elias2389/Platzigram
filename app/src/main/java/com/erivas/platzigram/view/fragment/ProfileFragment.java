674bf5e870b25b375bc9152752b35d5ccdf6a524
elias2303 <andreselias2303@gmail.com>
2017-11-19 12:59:11 -0400

    Se agrega circle image

--- app/src/main/java/com/erivas/platzigram/view/fragment/ProfileFragment.java
+++ app/src/main/java/com/erivas/platzigram/view/fragment/ProfileFragment.java
@@ -3,11 +3,19 @@ package com.erivas.platzigram.view.fragment;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.erivas.platzigram.R;
+import com.erivas.platzigram.adapter.PictureAdapterRecyclerView;
+import com.erivas.platzigram.model.Picture;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -24,7 +32,22 @@ public class ProfileFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+         View view = inflater.inflate(R.layout.fragment_profile, container, false);
+        showToolbar("",false, view);
+
+        return view;
+
     }
+
+
+
+
+    public void showToolbar(String title, boolean upButton, View view){
+        Toolbar toolbar = (Toolbar) view.findViewById(R.id.toolbar);
+        ((AppCompatActivity) getActivity()).setSupportActionBar(toolbar);
+        ((AppCompatActivity) getActivity()).getSupportActionBar().setTitle(title);
+        ((AppCompatActivity) getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(upButton);
+    }
+
 
 }
